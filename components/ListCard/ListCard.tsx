a1475e076da5b646af8cef4b5933a2cdeb401168
Marcel Lukomski <marcel.lukomski@gmail.com>
2023-02-17 19:22:13 +0100

    Add basic ListCard structure.

--- components/ListCard/ListCard.tsx
+++ components/ListCard/ListCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { jobData } from '../../features/jobDataSlice';
 
 interface ListCardProps {
@@ -5,8 +6,33 @@ interface ListCardProps {
 }
 const ListCard = ({ jobDetails }: ListCardProps) => {
   return (
-    <div>
-      <p>{jobDetails.languages}</p>
+    <div className='wrapper'>
+      <div className='col-1'>
+        <div className='logo-wrapper'>
+          <Image
+            src={jobDetails.logo}
+            alt={jobDetails.company}
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className='job-content'>
+          <p>{jobDetails.company}</p>
+          <p>{jobDetails.position}</p>
+          <ul>
+            <li>{jobDetails.postedAt}</li>
+            <li>{jobDetails.contract}</li>
+            <li>{jobDetails.location}</li>
+          </ul>
+        </div>
+      </div>
+      <div className='col-2'>
+        <p>{jobDetails.role}</p>
+        <p>{jobDetails.level}</p>
+        {jobDetails.languages.map((language, index) => (
+          <p key={index}>{language}</p>
+        ))}
+      </div>
     </div>
   );
 };
