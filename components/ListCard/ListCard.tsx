25c94bb796728ff6d1e48867c05f87a5e303a9a8
Marcel Lukomski <marcel.lukomski@gmail.com>
2023-02-18 13:16:38 +0100

    Add styles to ListCard component.

--- components/ListCard/ListCard.tsx
+++ components/ListCard/ListCard.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import { jobData } from '../../features/jobDataSlice';
+import styles from '../ListCard/ListCard.module.scss';
 
 interface ListCardProps {
   jobDetails: jobData;
 }
 const ListCard = ({ jobDetails }: ListCardProps) => {
   return (
-    <div className='wrapper'>
-      <div className='col-1'>
-        <div className='logo-wrapper'>
+    <div className={styles.wrapper}>
+      <div className={styles.col1}>
+        <div className={styles.logoWrapper}>
           <Image
             src={jobDetails.logo}
             alt={jobDetails.company}
@@ -16,17 +17,17 @@ const ListCard = ({ jobDetails }: ListCardProps) => {
             height={50}
           />
         </div>
-        <div className='job-content'>
+        <div className={styles.jobContent}>
           <p>{jobDetails.company}</p>
           <p>{jobDetails.position}</p>
-          <ul>
+          <ul className={styles.jobList}>
             <li>{jobDetails.postedAt}</li>
             <li>{jobDetails.contract}</li>
             <li>{jobDetails.location}</li>
           </ul>
         </div>
       </div>
-      <div className='col-2'>
+      <div className={styles.col2}>
         <p>{jobDetails.role}</p>
         <p>{jobDetails.level}</p>
         {jobDetails.languages.map((language, index) => (
